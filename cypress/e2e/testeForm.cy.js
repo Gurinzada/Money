8c72eb3ba4aaa84c13234b48e5461c3abcce822f
Gurinzada <augustoinacio243@gmail.com>
2023-11-23 22:59:38 -0300

    Arrumando comentarios

--- cypress/e2e/testeForm.cy.js
+++ cypress/e2e/testeForm.cy.js
@@ -1,28 +1,27 @@
 import { faker } from "@faker-js/faker";
 
-describe('User Registration and Login', () => {
+describe('Usuario Registro e Login', () => {
     let email = faker.internet.email();
     let password = faker.internet.password();
 
-  it('Registers a new user', () => {
-    // Use the imported email and password variables from the support/faker file
+  it('Registro e Logout', () => {
     cy.visit('http://localhost:3000/auth');
     cy.get('[data-cy=register-email-input]').type(email);
     cy.get('[data-cy=register-password-input]').type(password);
     cy.get('[data-cy=register-button]').click();
 
     cy.wait(2000);
-    cy.get('[data-cy=containerNav]').should('exist'); // Verify user is logged in
-    cy.get('[data-cy=logouth3]').click(); // Logout the user
+    cy.get('[data-cy=containerNav]').should('exist');
+    cy.get('[data-cy=logouth3]').click(); // Logout o usuario
   });
 
-  it('Logs in with the new user', () => {
+  it('Login com novo usuario', () => {
     cy.visit('http://localhost:3000/auth');
-    cy.get('[data-cy=login-email-input]').type(email); // Use the imported email variable
-    cy.get('[data-cy=login-password-input]').type(password); // Use the imported password variable
+    cy.get('[data-cy=login-email-input]').type(email); // 
+    cy.get('[data-cy=login-password-input]').type(password); //
     cy.get('[data-cy=login-button]').click();
 
-    cy.get('[data-cy=containerNav]').should('exist'); // Verify user is logged in
-    cy.get('[data-cy=logouth3]').should('contain', 'Logout'); // Verify logout link exists
+    cy.get('[data-cy=containerNav]').should('exist'); // 
+    cy.get('[data-cy=logouth3]').should('contain', 'Logout'); // 
   });
 });
